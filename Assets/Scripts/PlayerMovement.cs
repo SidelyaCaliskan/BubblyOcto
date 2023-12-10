ef86ccd54652f24bac4626c1336ba31906145e66
ardasenyuz <ardasenyuz@gmail.com>
2023-12-10 15:52:01 +0300

    fixed air jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,9 +11,12 @@ public class PlayerMovement : MonoBehaviour
     
     private Vector2 moveInput;
     private Rigidbody2D myRigidbody;
+    private CapsuleCollider2D myCapsuleCollider;
+    
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -29,6 +32,11 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
         if (value.isPressed)
         {
             myRigidbody.velocity += new Vector2(0f, jumpSpeed);
