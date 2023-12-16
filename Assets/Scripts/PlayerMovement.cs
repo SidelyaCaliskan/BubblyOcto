1f2665239395dbe5aa812830dff4d21d5e39bcb5
ardasenyuz <ardasenyuz@gmail.com>
2023-12-16 18:47:47 +0300

    Added spikes (hazards tilemap)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -81,7 +81,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Die()
     {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Dying");
