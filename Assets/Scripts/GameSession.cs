eef835c2274c67ce1d33a08c8e612a81b61783b7
ardasenyuz <ardasenyuz@gmail.com>
2023-12-17 23:17:18 +0300

    Added scene persistence
    coins and enemies won't reappear after we die

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -52,6 +52,7 @@ public class GameSession : MonoBehaviour
 
     void ResetGameSession()
     {
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
         //checkpoint de olabilir
         SceneManager.LoadScene(0);
         Destroy(gameObject);
